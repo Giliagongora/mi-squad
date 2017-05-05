6e64278038374f817ea2a37f33704b5f78e9765c
Gilia Gongora <giliagongora.g@gmail.com>
2017-05-04 22:41:40 -0300

    desarrollo html

---
+++ assets/main.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Valentina Lopez 25 tarot, idiomas, peliculas mudas
+
+Trinidad Fuentes 26 video juegos, pugLover, dormir
+
+Diana Monsalve 29  hacer scratch, musica, negocios
+
+Daniela Sanchez 24 escuchar musica, cantar, ver series
+
+Maria Paz Rojas 26 musica, comer, hablar ingles
+
+Gilia Gongora 28 
+Maribel Diaz 28 salir a tomar café y comer muffins , el silencio y escribir
+
+Valentina Toledo, 20, jugar fútbol, música, actividades al aire libre.
+
+Karina Palacios, 24, Ver series, compartir con amigos, leer y escuchar musica
+*/
